f0416a692a0adc7c2e0d73ce47b3a421b72fb36b
boran <borangoher@proton.me>
2025-05-03 14:10:18 +0300

    feat: Switch to literate doom config

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -1,14 +1,16 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
-
 (package! org-appear
   :recipe (:host github
            :repo "awth13/org-appear"))
-
 (package! org-super-agenda)
-
 (package! org-cv
   :recipe (:host gitlab
            :repo "Titan-C/org-cv"))
-
 (package! org-auto-tangle)
+(package! mixed-pitch)
+(package! org-modern)
+(package! toc-org)
+(package! nerd-icons-ibuffer)
+(package! nov)
+(package! djvu)
